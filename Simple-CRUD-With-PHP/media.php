19007efe8999a3a1a12add7c102e5408eaaafcb1
Irvanabdurrahman <irvan_chester@ymail.com>
2015-02-23 07:17:09 +0700

    Edit

--- Simple-CRUD-With-PHP/media.php
+++ Simple-CRUD-With-PHP/media.php
@@ -14,9 +14,9 @@ if($menu=='tambah'){
 		$editdata=mysql_query("SELECT * FROM daftar WHERE id='$_GET[id]'");
 		$ed=mysql_fetch_array($editdata);
 		echo "<tr><td><input type=hidden name=id value=$ed[id]>
-				Nama</td><td> : </td><td><input type='text' name='nama' value=$ed[nama]></td></tr>
-				<tr><td>Alamat</td><td> : </td><td><input type='text' name='alamat' value=$ed[alamat]></td></tr>
-				<tr><td>Tanggal Lahir</td><td> : </td><td><input type='date' name='tgl' value=$ed[tgl]></td></tr>
+				Nama</td><td> : </td><td><input type='text' name='nama' value='$ed[nama]'></td></tr>
+				<tr><td>Alamat</td><td> : </td><td><input type='text' name='alamat' value='$ed[alamat]'></td></tr>
+				<tr><td>Tanggal Lahir</td><td> : </td><td><input type='date' name='tgl' value='$ed[tgl]'></td></tr>
 				<tr><td  colspan='2'><input type='submit' value='OK'><input type='button' onclick=self.history.back() value='Batal'></td></tr>
 			</table></form>";
 
